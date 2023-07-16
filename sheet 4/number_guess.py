d7e799232518bd1b937ccfb764bd7496f2292c68
Aiden Romero <aiden0romero@gmail.com>
2023-07-16 12:59:11 -0400

    fully functional

--- sheet 4/number_guess.py
+++ sheet 4/number_guess.py
@@ -1,26 +1,31 @@
 from random import randint
 
-def guessing_game()
-print("I've thought of a random number between 1 and 1000")
-print()
-guess=0
-number = randint(1,1000)
-while True:
-    answer = int(input("Guess what that number is! "))
-    if answer<number:
-        guess = guess+1
-        print("Too low!")
-        print()
-    elif answer>number:
-        guess = guess+1
-        print("Too high!")
-        print()
-    else:
-        guess = guess+1
-        print("You got it!")
-        break
-print("Your number of guesses was", guess)
-
-playagain = input("Would you like to play again?")
-if playagain == "yes":
+def guessing_game():
+    print("I've thought of a random number between 1 and 1000")
     print()
+    guess=0
+    number = randint(1,1000)
+    while True:
+        answer = int(input("Guess what that number is! "))
+        if answer<number:
+            guess = guess+1
+            print("Too low!")
+            print()
+        elif answer>number:
+            guess = guess+1
+            print("Too high!")
+            print()
+        else:
+            guess = guess+1
+            print("You got it!")
+            print()
+            break
+    print("Your number of guesses was " + str(guess))
+    print()
+
+    playagain = input("Would you like to play again? ")
+    if playagain == "yes":
+        print()
+        guessing_game()
+
+guessing_game()
