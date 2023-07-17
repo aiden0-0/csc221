032c569e1fbad43d0f4333d749532c4fb82eac47
Aiden Romero <aiden0romero@gmail.com>
2023-07-17 09:34:50 -0400

    start sheet 5

---
+++ sheet5/robots.py
@@ -0,0 +1,18 @@
+from gasp import
+
+def place_player():
+    print("Here I am!")
+
+def move_player():
+    print("I'm moving...")
+    update_when('key_pressed')
+
+begin_graphics()
+finished=False
+
+place_player()
+
+while not finished:
+    move_player()
+
+end_graphics()
