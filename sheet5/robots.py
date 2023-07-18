6d9cf8ad8584dd901aee903837a9abf5ee5abd4c
Aiden Romero <aiden0romero@gmail.com>
2023-07-18 14:22:56 -0400

    moving player

--- sheet5/robots.py
+++ sheet5/robots.py
@@ -3,22 +3,28 @@ from random import randint
 player_x = randint(0,63)
 player_y = randint(0,47)
 
+
 def place_player():
-    print("Here I am!")
-    c = Circle((10 * player_x + 5, 10 * player_y + 5), 5, filled=True)
+    global c
+    c = Circle((10*player_x +5, 10*player_y +5), 5, filled=True)
 
 def move_player():
-    global c
-    c = move_to(c, (player_x, player_y ))
-    print("I'm moving...")
-    update_when('key_pressed')
+    global c, player_x, player_y
+    print('movin')
+    key = update_when('key_pressed')
+    
+    if key == 'Up' and player_y < 480:
+        player_y += 1
+    if key == 'Left' and player_x > 0:
+        player_x -= 1
+    if key == 'Down' and player_y > 0:
+        player_y -= 1
+    if key == 'Right' and player_x < 640:
+        player_x += 1
+    move_to(c, (player_x, player_y ))
+    remove_from_screen(c)
 
 begin_graphics()
-finished=False
-
-place_player()
-
-while not finished:
+while True:
+    place_player()
     move_player()
-
-end_graphics()
